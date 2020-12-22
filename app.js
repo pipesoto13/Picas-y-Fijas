29c12c3205b96440d776317b2cf7815bb1a5a2a0
Felipe Soto Correa <pipesoto13@gmail.com>
2020-12-22 16:52:42 -0500

    Add winner modal and comments

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-//Get number between 0 and 9.
+//Function to get number between 0 and 9.
 function getRandomNumber() {
     return Math.floor(Math.random() * (10));
 };
 
 $(document).ready(function() {
-    let digits = [8,0,4,7];
-/*     for (let i = 0; i < 4; i++) {
+    let digits = [];
+    for (let i = 0; i < 4; i++) {
         let digit = getRandomNumber();
 
-        //Get other number if exist in array
+        //Get another number if it exists in array
         do {
             digit = getRandomNumber(); 
         } while (digits.includes(digit));
 
         //Save number in array
         digits.push(digit);
-    } */
+    }
     
+    //Juts for display number in console and check
     const number = digits.join('');
     console.log(number);
     
@@ -27,6 +28,8 @@ $(document).ready(function() {
             const repeatedNumber =  arrOfNumber.some(function(v,i,a){
                 return a.lastIndexOf(v)!=i;
               });
+            
+            // Validate conditions to continue (4 digits number and no repeated)
             if ((inputNumber.length === 4) && (!repeatedNumber)) {
                 $("input").css("outline-color", "#84b3ed");
                 let contPicas = 0;
@@ -46,16 +49,28 @@ $(document).ready(function() {
                         <td>${contFijas}</td>
                     </tr>
                 `
+                //Add row with info
                 $("table tbody").prepend(template);
-    
+                
+                //Erase input field
                 $("#number").val("");
+
+                //Open modal with winner message
+                if (contFijas === 4) {
+                    $("#myModal").css("display","flex");
+                }
+
+                //Close modal and reload
+                $(".modal-content button").on("click", function(){
+                    $("#myModal").css("display","none");
+                    location.reload();
+                })
                 
             } else {
+                //Display alert in validation error
                 $("input").css("outline-color", "red");
                 alert("Ingrese un número de 4 dígitos y que no tenga dígitos repetidos")
-
             }
-
         }
     })
 })
