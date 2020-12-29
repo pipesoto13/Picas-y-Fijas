4dd5cdca41998069d1d951ce3410a2c63a36f6ae
Felipe Soto Correa <pipesoto13@gmail.com>
2020-12-28 19:22:31 -0500

    Add validation color

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ function getRandomNumber() {
     return Math.floor(Math.random() * (10));
 };
 
-$(document).ready(function() {
+//Function for four digits number without repeating 
+function getFourDigitsNumber() { 
     let digits = [];
     for (let i = 0; i < 4; i++) {
         let digit = getRandomNumber();
@@ -15,16 +16,23 @@ $(document).ready(function() {
 
         //Save number in array
         digits.push(digit);
+
     }
-    
     //Juts for display number in console and check
     const number = digits.join('');
     console.log(number);
+    return number;
+}
+
+$(document).ready(function() {
+    //Call functions for four digits number
+    let number = getFourDigitsNumber();
     
     $("#number").on("keypress", function(e) {
         if (e.which === 13) {
             const inputNumber = $("#number").val();
             const arrOfNumber = inputNumber.split('');
+            //Verify repeated number in array
             const repeatedNumber =  arrOfNumber.some(function(v,i,a){
                 return a.lastIndexOf(v)!=i;
               });
@@ -32,6 +40,7 @@ $(document).ready(function() {
             // Validate conditions to continue (4 digits number and no repeated)
             if ((inputNumber.length === 4) && (!repeatedNumber)) {
                 $("input").css("outline-color", "#84b3ed");
+                $("label span").css("color", "black");
                 let contPicas = 0;
                 let contFijas = 0;
                 arrOfNumber.forEach((element,i) => {
@@ -58,18 +67,21 @@ $(document).ready(function() {
                 //Open modal with winner message
                 if (contFijas === 4) {
                     $("#myModal").css("display","flex");
+                    $("input").prop( "disabled", true ); //Disable input
                 }
 
                 //Close modal and reload
                 $(".modal-content button").on("click", function(){
                     $("#myModal").css("display","none");
+                    $("input").prop( "disabled", false ); //Enable input
+                    $("table tbody").children().remove();
                     location.reload();
                 })
                 
             } else {
-                //Display alert in validation error
+                //Display red colors in validation error if number do not match validations
                 $("input").css("outline-color", "red");
-                alert("Ingrese un número de 4 dígitos y que no tenga dígitos repetidos")
+                $("label span").css("color", "red");
             }
         }
     })
